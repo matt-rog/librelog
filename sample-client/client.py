377e7df2a2528e528cadf869cab20344be62e620
Matthew Rogers <alula_irl@proton.me>
2025-03-05 19:00:33 -0600

    init py client

---
+++ sample-client/client.py
@@ -0,0 +1,23 @@
+import websockets
+import asyncio
+import psutil
+import time
+
+
+async def ws_client():
+    url = "ws://127.0.0.1:9000/injest"
+
+    # Connect to the server
+    async with websockets.connect(url) as ws:
+        print("WebSocket: Client Connected.")
+
+        # Send memory usage every second
+        while True:
+            time.sleep(1)
+            await ws.send(str(psutil.virtual_memory().percent))
+            # msg = await ws.recv()
+            # print(msg)
+
+
+# Start the connection
+asyncio.run(ws_client())
