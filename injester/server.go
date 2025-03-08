0f8d465020895b9c36f61c068f8b0ac40280ab3c
Matthew Rogers <alula_irl@proton.me>
2025-03-07 23:59:34 -0600

    write logs to cass

--- injester/server.go
+++ injester/server.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"log"
 	"net/http"
+	"os"
+	"strings"
+	"time"
 
+	"github.com/gocql/gocql"
 	"github.com/gorilla/websocket"
 )
+
+type LogObject struct {
+    UserID string          `json:"user_id"`
+    LogSet string          `json:"log_set"`
+    Data   json.RawMessage `json:"data"`
+}
 
 var addr = flag.String("addr", "localhost:9000", "http service address")
 
 var upgrader = websocket.Upgrader{}
+
+var session *gocql.Session
 
 func injest(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
@@ -23,18 +36,50 @@ func injest(w http.ResponseWriter, r *http.Request) {
 		mt, message, err := c.ReadMessage()
 		if err != nil {
 			log.Println("read:", err)
-			break
+			continue
 		}
+	
+		var lo LogObject
+		err = json.Unmarshal([]byte(message), &lo)
+		if err != nil {
+			log.Println("invalid json:", err)
+			c.WriteMessage(mt, []byte("invalid json"))
+			continue
+		}
+
+		currentTime := time.Now().UnixMilli()
+
+		err = session.Query(`INSERT INTO logs (user_id, log_set, timestamp, data) VALUES (?,?,?,?)`, lo.UserID, lo.LogSet, currentTime, lo.Data).Exec();
+		if err != nil {
+        	log.Println("insert error:", err)
+			c.WriteMessage(mt, []byte("insert error"))
+			continue
+    	}
+
 		log.Printf("recv: %s", message)
 		err = c.WriteMessage(mt, message)
 		if err != nil {
 			log.Println("write:", err)
-			break
+			continue
 		}
 	}
 }
 
 func main() {
+
+	urls := strings.Split(os.Getenv("CASSANDRA_CLUSTER"), " ")
+	cluster := gocql.NewCluster(urls...)
+	cluster.Keyspace = "librelog"
+	cluster.Consistency = gocql.Quorum
+
+	var err error
+	session, err = cluster.CreateSession()
+	if err != nil {
+		log.Fatal("Failed to connect to Cassandra:", err)
+	}
+	defer session.Close() 
+
+
 	flag.Parse()
 	log.SetFlags(0)
 	http.HandleFunc("/injest", injest)
